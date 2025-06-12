c5e2aeb2bd2ab870204ab8759b29b4434607fda9
This PC <luunhut369@gmail.com>
2025-06-12 16:16:50 +0700

    Add project files.

---
+++ WebApi/Model/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>
+     options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
